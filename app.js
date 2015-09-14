9ba449b7623155331af0049450d56d27ceba9d78
Benjamin Goh <bengoh@paypal.com>
2015-09-13 18:37:01 -0700

    Doesn't work because react-router needs a DOM

---
+++ app.js
@@ -0,0 +1,4 @@
+// Everything here onwards can be ES6 / JSX
+const three = () => 3;
+
+console.log(three());
---
+++ src/raw.js
@@ -0,0 +1,64 @@
+// taken from https://github.com/rackt/react-router/blob/master/docs/introduction/README.md
+import React from 'react';
+
+import { Router, Route, Link } from 'react-router';
+
+const About = React.createClass({
+	render() {
+		return (
+			<div>
+				<h2>This is an about!</h2>
+				<p>About meeeeee....!!!!</p>
+			</div>
+		);
+	}
+});
+
+const Message = React.createClass({
+	render() {
+		const props = this.props;
+		const params = props.params;
+
+		return <h3>Message {params.id}</h3>;
+	}
+});
+
+const Inbox = React.createClass({
+	render() {
+		return (
+			<div>
+				<h2>Inbox</h2>
+				{this.props.children || <p>Welcome to your inbox</p>}
+			</div>
+		);
+	}
+});
+
+const App = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1>App</h1>
+				<ul>
+					<li><Link to="/about">About</Link></li>
+					<li><Link to="/inbox">Inbox</Link></li>
+				</ul>
+
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+export default function run(body) {
+	return React.renderToString((
+		<Router>
+			<Route path="/" component={App}>
+				<Route path="about" component={About} />
+				<Route path="inbox" component={Inbox}>
+					<Route path="messages/:id" component={Message} />
+				</Route>
+			</Route>
+		</Router>
+	));	
+}
