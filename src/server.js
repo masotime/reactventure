e203328fb9b4944bfc484c84f69821e840c3f151
Benjamin Goh <bengoh@paypal.com>
2015-10-11 23:40:37 -0700

    Attempting to implement authentication

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import routes from './routes';
 import render from './server/render';
 
-console.log(`Initializing...`);
-
 // all the webpack-hot-middleware stuff
 import webpack from 'webpack';
 import devMiddleware from 'webpack-dev-middleware';
 import hotMiddleware from 'webpack-hot-middleware';
+
+console.log(`Server initializing...`);
 
 const app = express();
 
@@ -22,8 +22,8 @@ const ignite = (app, config) => {
 	app.use(hotMiddleware(compiler));
 }
 
-// define a route module to be used as middleware
-const routerMiddleware = ((router) => {
+// these are basic routes, without security
+const basicRoutes = ((router) => {
 	router.get('/', (req, res, next) => {
 		console.log('Index page');
 		next();
@@ -34,28 +34,80 @@ const routerMiddleware = ((router) => {
 		next();
 	});
 
-	router.get('/inbox', (req, res, next) => {
-		console.log('Inbox page');
-		next();
-	});
-
 	router.get('/login', (req, res, next) => {
 		console.log('Login page');
 		next();
-	})
+	});
+
 
 	return router;
 })(express.Router());
+
+// we introduce secured routes. 
+// see https://auth0.com/blog/2015/09/28/5-steps-to-add-modern-authentication-to-legacy-apps-using-jwts/
+import { authenticate, generate } from './lib/jwt';
+import moment from 'moment';
+const securedRoutes = ((router, generate, authenticate) => {
+
+	// this route does the acutal login stuff
+	// NOTE: This doesn't check to see if the user_id is actually an existing one!
+	router.post('/login', (req, res) => {
+		const id_token = generate({ user_id: req.body.user_id });
+		res.json({ id_token });
+	});
+
+	// this route checks the header for a valid jwt and 
+	// sets an appropriate cookie. "authenticate" adds
+	// the token to the body which we will retrieve
+	router.post('/authorize-cookie', authenticate, (req, res) => {
+		res.cookie('id_token', req.body.token, {
+			expires: moment().add(1, 'hour'),
+			httpOnly: true
+		});
+		res.json({ message: 'Cookie set!' });
+	});
+
+	router.get('/users', authenticate, (req, res, next) => {
+		console.log('Users page');
+		next();
+	});
+
+	router.get('/posts', authenticate, (req, res, next) => {
+		console.log('Posts page');
+		next();
+	});
+
+	router.get('/media', authenticate, (req, res, next) => {
+		console.log('Media page');
+		next();
+	});
+
+	router.get('/messages', authenticate, (req, res, next) => {
+		console.log('Messages page');
+		next();
+	});
+
+	return router;
+
+})(express.Router(), generate, authenticate);
 
 // add the hot middleware into the stack
 import webpackConfig from '../webpack.config';
 ignite(app, webpackConfig);
 
+// add cookies and body-parser (JSON and text) into the stack
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
 // we serve static files (really just the webpack bundle)
 app.use(express.static('build/public'));
 
 // we delegate rendering to a universal react catchall
-app.use(routerMiddleware);
+app.use(basicRoutes);
+app.use(securedRoutes);
 app.use('/*', (req, res) => {
 	// DON'T USE req.url, it's part of http not express
 	render({ routes, location: req.originalUrl, state: req.session.state}, (err, result) => {
