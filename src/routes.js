8360a636b88d64f561159906b1d18c91a44f1c08
Benjamin Goh <bengoh@paypal.com>
2015-10-13 07:25:55 -0700

    Universal authentication

--- src/routes.js
+++ src/routes.js
@@ -7,16 +7,26 @@ import { About, Dashboard, Login, UsersPage, MessagesPage, MediasPage, PostsPage
 // we also need a master "App" layout
 import App from './app';
 
+// this is a universal function. It _only_ checks for authentication
+// it does not verify the token, if any. You cannot make data loading
+// univesal because you cannot dump unauthorized data client-side, which
+// is an obvious breach of security
+const requireAuth = (nextState, replaceState) => {
+
+}
+
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Dashboard} />
 		<Route path="dashboard" component={Dashboard} />
 		<Route path="about" component={About} />
 		<Route path="login" component={Login} />
-		<Route path="users" component={UsersPage} />
-		<Route path="messages" component={MessagesPage} />
-		<Route path="medias" component={MediasPage} />
-		<Route path="posts" component={PostsPage} />
+
+		{/* These are protected routes */}
+		<Route path="users" component={UsersPage} onEnter={requireAuth} />
+		<Route path="messages" component={MessagesPage} onEnter={requireAuth} />
+		<Route path="medias" component={MediasPage} onEnter={requireAuth} />
+		<Route path="posts" component={PostsPage} onEnter={requireAuth} />
 	</Route>
 );
 
